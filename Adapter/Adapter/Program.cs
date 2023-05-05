9fcb946ce87edfffc6ac736549ff328159f9e19b
cristinel <cristian.harea@gmail.com>
2023-05-05 17:56:18 +0300

    first commit

---
+++ Adapter/Adapter/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Transactions;
+
+namespace ExpenseTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            TransactionListAdapter transactionList= new TransactionListAdapter();
+
+            while (true)
+            {
+                Console.WriteLine("Expense Tracker");
+                Console.WriteLine("----------------");
+
+                Console.WriteLine("1. Add expense");
+                Console.WriteLine("2. Add income");
+                Console.WriteLine("3. View transactions");
+                Console.WriteLine("4. Export report");
+                Console.WriteLine("5. Clear all transactions");
+                Console.WriteLine("6. Exit");
+
+                Console.Write("\nEnter your choice: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        // Add expense
+                        transactionList.AddTransaction(TransactionType.Expense);
+                       
+                        break;
+
+                    case "2":
+
+                        transactionList.AddTransaction(TransactionType.Income);
+
+                        break;
+
+                    case "3":
+                        // View transactions
+                        transactionList.ViewTransactions();
+
+                        break;
+
+                    case "4":
+                        // Export report                     
+                        transactionList.GenerateTextReport();
+
+                        break;
+
+                    case "5":
+                        // Clear all transactions
+                        transactionList.ClearTransactions();
+                      
+                        break;
+
+                    case "6":
+                        // Exit the application
+                        Console.WriteLine("Thank you for using Expense Tracker!");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid input. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+
+    // Define the Transaction class
+    class Transaction
+    {
+        public decimal Amount { get; }
+        public string Category { get; }
+        public TransactionType Type { get; }
+
+        public Transaction(decimal amount, string category, TransactionType type)
+        {
+            Amount = amount;
+            Category = category;
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"{Category}: {Amount}$ ({Type})";
+        }
+    }
+
+    class TransactionList
+    {
+        public List<Transaction> transactions { get; set; }
+
+        public TransactionList()
+        {
+            transactions = new List<Transaction>();
+        }
+
+        public void AddTransaction(TransactionType type)
+        {
+            Console.Write("Enter amount: ");
+            var expenseAmount = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter category: ");
+            var expenseCategory = Console.ReadLine();
+
+            transactions.Add(new Transaction(expenseAmount, expenseCategory, type));
+
+            Console.WriteLine("Transaction added successfully.");
+        }
+
+        public void ClearTransactions()
+        {
+            transactions.Clear();
+
+            Console.WriteLine("All transactions cleared.");
+        }
+
+        public void ViewTransactions()
+        {
+            Console.WriteLine("Transactions:");
+            foreach (var transaction in transactions)
+            {
+                Console.WriteLine(transaction);
+            }
+        }
+    }
+
+    // Define the TransactionType enum
+    enum TransactionType
+    {
+        Expense,
+        Income
+    }
+
+    // Define the IAccountingSystemAdapter interface
+    interface IExpenseTrackerAdapter
+    {
+        void GenerateTextReport();
+    }
+
+    // Define the QuickBooksAdapter class
+    class TransactionListAdapter : TransactionList, IExpenseTrackerAdapter
+    {
+        public TransactionListAdapter() : base()
+        {
+
+        }
+
+        public void GenerateTextReport()
+        {
+
+
+            Console.Write("Enter file name: ");
+            string fileName = Console.ReadLine();
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(fileName))
+            {
+                decimal totalExpenses = 0;
+                decimal totalIncome = 0;
+
+                file.WriteLine("Expense Tracker Report");
+                file.WriteLine("-----------------------");
+                file.WriteLine("Transactions:");
+
+                foreach (var transaction in transactions)
+                {
+                    if (transaction.Type == TransactionType.Expense)
+                    {
+                        file.WriteLine($"Expense: {transaction.Category} - {transaction.Amount:C}");
+                        totalExpenses += transaction.Amount;
+                    }
+                    else
+                    {
+                        file.WriteLine($"Income: {transaction.Category} - {transaction.Amount:C}");
+                        totalIncome += transaction.Amount;
+                    }
+                }
+
+                file.WriteLine("");
+                file.WriteLine($"Total Expenses: {totalExpenses:C}");
+                file.WriteLine($"Total Income: {totalIncome:C}");
+                file.WriteLine($"Net Balance: {(totalIncome - totalExpenses):C}");
+            }
+        }
+    }
+}
---
+++ Adapter/CompositeDecorator/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RecipeManagementSystem
+{
+    // Component interface
+    public interface IIngredient
+    {
+        string Name { get; }
+        string Description { get; }
+        double Price { get; }
+        void Print();
+    }
+
+    // Leaf class
+    public class Ingredient : IIngredient
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public double Price { get; }
+
+        public Ingredient(string name, string description, double price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Name} - {Description} (${Price:F2})");
+        }
+    }
+
+    // Decorator abstract class
+    public abstract class IngredientDecorator : IIngredient
+    {
+        protected IIngredient _ingredient;
+
+        public IngredientDecorator(IIngredient ingredient)
+        {
+            _ingredient = ingredient;
+        }
+
+        public string Name => _ingredient.Name;
+        public string Description => _ingredient.Description;
+        public double Price => _ingredient.Price;
+        public abstract void Print();
+    }
+
+    // Concrete decorator class
+    public class OrganicDecorator : IngredientDecorator
+    {
+        public OrganicDecorator(IIngredient ingredient) : base(ingredient)
+        {
+        }
+
+        public override void Print()
+        {
+            _ingredient.Print();
+            Console.WriteLine("- organic");
+        }
+    }
+
+    // Concrete decorator class
+    public class GlutenFreeDecorator : IngredientDecorator
+    {
+        public GlutenFreeDecorator(IIngredient ingredient) : base(ingredient)
+        {
+        }
+
+        public override void Print()
+        {
+            _ingredient.Print();
+            Console.WriteLine("- gluten-free");
+        }
+    }
+
+    // Composite class
+    public class Recipe : IIngredient
+    {
+        protected readonly List<IIngredient> _components = new List<IIngredient>();
+
+        public string Name { get; }
+        public string Description { get; }
+        public double Price { get; private set; }
+
+        public Recipe(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public void AddComponent(IIngredient component)
+        {
+            _components.Add(component);
+            Price += component.Price;
+        }
+
+        public void RemoveComponent(IIngredient component)
+        {
+            _components.Remove(component);
+            Price -= component.Price;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Name} - {Description} (${Price:F2})");
+            Console.WriteLine("Contents:\n");
+            foreach (var component in _components)
+            {
+                component.Print();
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public class Menu : Recipe
+    {
+        public Menu(string name, string description) : base(name, description)
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create some ingredients
+            var flour = new Ingredient("Flour", "Organic flour", 2.0);
+            var sugar = new Ingredient("Sugar", "White sugar", 1.5);
+            var eggs = new Ingredient("Eggs", "Free-range eggs", 3.0);
+
+            // Decorate ingredients with additional functionality
+            var organicFlour = new OrganicDecorator(flour);
+            var glutenFreeFlour = new GlutenFreeDecorator(flour);
+            var organicSugar = new OrganicDecorator(sugar);
+
+            // Create a recipe
+            var cake = new Recipe("Cake", "A delicious cake");
+            cake.AddComponent(organicFlour);
+            cake.AddComponent(organicSugar);
+            cake.AddComponent(eggs);
+
+            // Create another recipe
+            var cookies = new Recipe("Cookies", "Some tasty cookies");
+            cookies.AddComponent(glutenFreeFlour);
+            cookies.AddComponent(sugar);
+            cookies.AddComponent(eggs);
+
+            // Add the recipes to a menu
+            var menu = new Menu("Menu", "Our menu");
+            menu.AddComponent(cake);
+            menu.AddComponent(cookies);
+
+            // Print the menu
+            menu.Print();
+
+        }
+    }
+}
